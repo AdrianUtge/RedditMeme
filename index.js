b29c47117eb2ef1cc768c8d0e16767112f43f74f
Adrian Utge <adrianutge@gmail.com>
2021-09-21 08:45:16 +0200

    algebra js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const img = require('./assets/assets.jepg')
+const jimp = require('jimp')
+const algebra = require('algebra.js')
+
+jimp.read("./assets/assets.jpeg", (err, image) => {
+    if (err) throw err;
+    image.resize(256, 256)
+
+    image.greyscale().write("./assets/image.png");
+});
